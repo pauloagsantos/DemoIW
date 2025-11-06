0a13805921369e67cf078585aa018e230e593156
Paulo Santos <pauloagsantos@gmail.com>
2025-11-06 10:51:10 +0000

    testes Bootstrap

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ListaCarros from './Componentes/ListaCarros';
+import Teste from './Componentes/Teste';
 
 function App() {
   return (
---
+++ demo/src/Componentes/Teste.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Teste() {
+
+    return (
+        <>
+        <Container fluid className="p-5 bg-primary text-white text-center">
+             <h1>My First Bootstrap Page</h1>
+             <p>Resize this responsive page to see the effect!</p> 
+         </Container>
+
+        <Container className="mt-5">
+             <Row>
+                <Col sm={4}>
+                    <h3>Column 1</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                </Col>
+                <Col sm={4}>
+                    <h3>Column 2</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                </Col>
+                <Col sm={4}>
+                    <h3>Column 3</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                </Col>
+             </Row> 
+         </Container>
+        
+
+        <div className="container-fluid p-5 bg-primary text-white text-center">
+  <h1>My First Bootstrap Page</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+  
+<div className="container mt-5">
+  <div className="row">
+    <div className="col-sm-4">
+      <h3>Column 1</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div className="col-sm-4">
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div className="col-sm-4">
+      <h3>Column 3</h3>        
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+
+
+        
+        </>
+    )
+};
+
+export default Teste;
